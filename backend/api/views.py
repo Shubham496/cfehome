a81f5c9b55499a478627d7e850fa10b6926d6ed1
Shubham <iatco5h@gmail.com>
2024-11-28 02:06:59 +0530

    adding products app for models

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,9 +1,22 @@
 from django.shortcuts import render
+import json
 
 # Create your views here.
 
 from django.http import JsonResponse
 
 def api_home(request, *args, **kwargs):
-    return JsonResponse({"message": "hi there this is a api response"})
+    result = request.body
+ 
+    data = {}
+    try:
+        data = json.loads(result)
+    except json.JSONDecodeError:
+        data = {"error" : "invalid json data"}
+
+    data['headers'] = dict(request.headers)
+    data['content_type'] = request.content_type  # Correct content_type attribute
+    data["params"] = dict(request.GET)
+    
+    return JsonResponse(data)
 
--- py_client/basic.py
+++ py_client/basic.py
@@ -5,8 +5,8 @@ endpoint = "http://127.0.0.1:8000/api/"
 
 
 
-get_response = requests.get(endpoint)
+get_response = requests.get(endpoint, params={'abc': 123}, json={"query": "hello world"})
 
 
-print(get_response.json()["message"])
+print(get_response.json())
 
