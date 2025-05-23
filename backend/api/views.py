2aedd7c902c65d4420c7f2aa29cc54ea33c98818
Shubham <iatco5h@gmail.com>
2025-05-23 15:04:30 +0530

    understanding restframework

--- backend/api/views.py
+++ backend/api/views.py
@@ -2,13 +2,19 @@
 
 from django.http import JsonResponse
 import json
-
 from django.forms.models import model_to_dict
 from products.models import Product
 
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
+from products.serializers import ProductSerializer
+
+@api_view(['POST'])
 def api_home(request, *args, **kwargs):
-    model_data = Product.objects.all().order_by("?").first()
-    data = {}
-    if model_data:
-        data = model_to_dict(model_data, fields=['id', 'data'])
-    return JsonResponse(data)
+    data = request.data
+    serializer = ProductSerializer(data=request.data)
+    if serializer.is_valid():
+        # data = serializer.save()
+        print(serializer.data)
+        return Response(serializer.data)
--- py_client/basic.py
+++ py_client/basic.py
@@ -2,7 +2,7 @@ import requests
 
 endpoint = "http://localhost:8000/api/"
 
-get_response =  requests.get(endpoint, json={'query': "hello world"}, params={'abc':123}) # API > Application programming interface
+get_response =  requests.post(endpoint, json={'title': "hello world"}, params={'abc':123}) # API > Application programming interface
 
 # REST APIS - > WEB API
 # print(get_response.text) # raw text
